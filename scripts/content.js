97695874ca4eece02a1d6046abe599b9d85d5d6c
Cami Rodriguez <camiepisode@outlook.com>
2018-05-15 17:08:21 -0500

    upgraded bootstrap, managing options

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,9 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  
   console.log('Changing data from chrome extension: ' + request.data);
   var data = request.data || {};
 
-  /* var linksList = document.querySelectorAll('a');
-  [].forEach.call(linksList, function(header) {
-      header.innerHTML = request.data;
-  }); */
-
-  writeMessage({text: request.data});
+  writeMessage({ text: request.data });
   sendMessage();
 
   sendResponse({ data: data, success: true });
@@ -18,20 +14,20 @@ function writeMessage(args) {
   if (args && args.text) {
     console.log('Writting message: ' + args.text);
     var input = document.querySelector('#main [contenteditable~=true]');
-    if(input){
+    if (input) {
       input.innerHTML = args.text;
-    input.dispatchEvent(new Event('input', { bubbles: true }));
+      input.dispatchEvent(new Event('input', { bubbles: true }));
     } else {
       console.warn('This tab is not a Whatsapp web application')
     }
-    
+
   }
 }
 
 function sendMessage(args) {
   console.log('Sending message');
   var button = document.querySelector('button>span[data-icon="send"]').parentElement;
-  if(button){
+  if (button) {
     button.click();
   } else {
     console.warn('This tab is not a Whatsapp web application')
--- scripts/extension.js
+++ scripts/extension.js
@@ -1,19 +1,67 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('status').textContent = "Extension loaded";
-    var button = document.getElementById('changelinks');
-    button.addEventListener('click', function () {
-        $('#status').html('Clicked change links button');
-        var text = $('#linkstext').val();
-        if (!text) {
-            $('#status').html('Invalid text provided');
-            return;
-        }
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {data: text}, function(response) {
-                $('#status').html('changed data in page');
-                console.log('success');
-            });
-        });
-    });
+var whTab = null;
+document.addEventListener('DOMContentLoaded', function () {
+
+
 });
 
+window.onload = function () {
+  chrome.tabs.query({}, function (tabs) {
+    tabs.forEach(tab => {
+      var url = tab.url;
+
+      if (url.includes('web.whatsapp.com')) {
+        whTab = tab
+      }
+    });
+  })
+
+  chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
+    var url = tabs[0].url;
+
+    if (url.includes('web.whatsapp.com')) {
+      startApp()
+    } else {
+      denyUsage()
+    }
+  });
+}
+
+
+let startApp = () => {
+  var button = document.getElementById('changelinks');
+  document.getElementById('status').textContent = "Extension loaded";
+  $('.jump-actions').hide();
+  $('.allowed-actions').show;
+  button.addEventListener('click', function (e) {
+    e.preventDefault()
+    $('#status').html('Clicked change links button');
+    var text = $('#content-input').val();
+    if (!text) {
+      $('#status').html('Invalid text provided');
+      return;
+    }
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { data: text }, function (response) {
+        $('#status').html('changed data in page');
+        console.log('success');
+      });
+    });
+  });
+}
+
+let denyUsage = () => {
+  $('#status').html('Current website seems to be not Whatsapp web');
+  $('.allowed-container').hide();
+  $('.allowed-actions').hide();
+  if (whTab) {
+    var button = document.getElementById('jump-to-whatsapp');
+    button.addEventListener('click', function (e) {
+      e.preventDefault();
+      chrome.tabs.update(whTab.id, { highlighted: true });
+    })
+    $('.jump-actions').show();
+  } else {
+    $('.jump-actions').hide();
+  }
+
+}
